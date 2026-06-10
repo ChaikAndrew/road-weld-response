ed52a3854ac2dae3d823f89a25167514d365adeb
Andrii <andreychaika87@gmail.com>
2026-06-10 19:17:56 +0200

    Google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -123,6 +123,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-979662197" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-979662197');
+            `,
+          }}
+        />
+      </head>
       <body className={`${audiowide.variable} ${play.variable} font-sans antialiased`}>
         <script
           type="application/ld+json"
